ddc72369634be63b621d5d00f0b32e303c120a02
sheikhsuleman123 <mssheikh333@gmail.com>
2019-03-19 18:07:31 +0500

    second class sending json in response

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-let express = require ('express');                // Require or import the Express Library
-let app = express();                              // Calling the Express Library
+let express = require ('express');
 let bodyParser = require('body-parser');
-app.use(bodyParser.json())
-app.post('/home',function(req, res){
-res.end();
+
+let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.post('/home',function(req,res){
+    res.json({a:1})
 })
-
 app.use(express.static('./'));
-
-app.listen(8080);                                 // Setting the starting port
+app.listen(4242);
